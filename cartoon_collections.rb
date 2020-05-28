2208336430f2902fea4bd1ef127e282c4a54add2
Robert Defendini <robdefendini@gmail.com>
2020-05-28 19:12:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,18 @@
+#### My Code ####
 def roll_call_dwarves(dwarfs)# code an argument here
   dwarfs.each_with_index do | dwarf , index |
     puts "#{index + 1} #{dwarf}"
   end
 end
-
+####
 def summon_captain_planet(veggies) # code an argument here
   # Your code here
   #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-veggies.collect do |call| 
+  veggies.collect do |call| 
     call.capitalize + "!"
   end
 end
-
+####
 def long_planeteer_calls# code an argument here
   # Your code here
 end
