280ce4578ef1fd37a84139c0e137e9dee6460340
Robert Defendini <robdefendini@gmail.com>
2020-05-28 19:32:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
   # short_words = ["puff", "go", "two"]
   calls_long.map do |calls|
-    calls.size < 4 
+    if calls.size < 4
+      return true 
   end
 end
 
