3ee3181bd64fa9cb8beeadd2b9dad38b87033916
Robert Defendini <robdefendini@gmail.com>
2020-05-28 19:17:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(veggies) # code an argument here
   end
 end
 ####
-def long_planeteer_calls(array)# code an argument here
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
   if
 else
